96b186b9e2478d9cb915e2f76ed607fd768e1e43
Carolina Turner <carolinanturner@gmail.com>
2023-11-15 12:12:35 -0500

    testing

--- public/script.js
+++ public/script.js
@@ -1,54 +1,205 @@
 //Carolina Turner CSCE 242
-const getJSON = async ()=>{
+const getBeverages= async ()=>{
     try{
-        let response = await fetch("https://assignment14.onrender.com/api/beverages");
-       return await response.json();
-    }   catch (error){
-    console.log("could not retrieve json");
-    return "";
+        return (await fetch("https://assignment14.onrender.com/api/beverages/")).json();
+    }   catch(error){
+        console.log(error);
+        return "";
     }
 }
 
 const showBeverages = async () => {
-const beverageJSON = await getJSON();
-    if(beverageJSON == ""){
-        return ;
-    }
-    let beverageDiv= document.getElementById("beverages-list");
-
-    beverageJSON.forEach((beverage)=>{
-        let section = document.createElement("section");
-        beverageDiv.append(section);
-        let title=beverage.title;
-
-        let h3=document.createElement("h3");
-        section.append(h3);
-        h3.innerHTML = beverage.title;
-
-        let img = document.createElement("img");
-        section.append(img);
-        img.src="https://assignment14.onrender.com/" + beverage.img;
-
-        let ul = document.createElement("ul");
-        section.append(ul);
-        ul.append(getLi(`Hot or Iced: ${beverage.hot_or_iced}`));
-        ul.append(getLi(`Price ${beverage.price}`));
-        ul.append(getLi(`Fan Favorite?: ${beverage.fan_favorite}`));
-        ul.append(getLi(`We recommend trying: ${beverage. recommendation}`));
-        ul.append(getLi(`Available Flavors (updated daily) ${beverage.flavors}`));
+    let beverages=await getBeverages();
+    let beveragesDiv = document.getElementById("beverages-list");
+    beveragesDiv.innerHTML="";
+    beverages.forEach((beverage)=>{
+        const section = document.createElement("section");
         section.classList.add("beverage");
-    
-        return section;
-    
-    });
-}
- // get 
-const getLi = data=>{
-    const li =document.createElement("li");
-    li.textContent= data;
-    return li;
-}
+        beveragesDiv.append(section);
 
-window.onload = () =>{
+        const a = document.createElement("a");
+        a.href = "#";
+        section.append(a);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = beverage.title;
+        a.append(h3);
+
+        a.onclick = (e) => {
+            e.preventDefault();
+            displayDetails(beverage);
+        };
+    });
+};
+
+const displayDetails = (beverage) => {
+  const beverageDetails = document.getElementById("beverage-details");
+  beverageDetails.innerHTML = "";
+
+  const h3 = document.createElement("h3");
+  h3.innerHTML = beverage.title;
+  beverageDetails.append(h3);
+
+  const dLink = document.createElement("a");
+  dLink.innerHTML = "	&#x2715;";
+  beverageDetails.append(dLink);
+  dLink.id = "delete-link";
+
+  const eLink = document.createElement("a");
+  eLink.innerHTML = "&#9998;";
+  beverageDetails.append(eLink);
+  eLink.id = "edit-link";
+  
+  const priceP = document.createElement("p");
+  beverageDetails.append(priceP);
+  priceP.innerHTML = "Price:  "+beverage.price;
+
+  const hot_or_icedP = document.createElement("p");
+  beverageDetails.append(hot_or_icedP);
+  hot_or_icedP.innerHTML ="Hot or Iced?  " +beverage.hot_or_iced;
+
+  const flavorsP = document.createElement("p");
+  beverageDetails.append(flavorsP);
+  flavorsP.innerHTML = "Flavors:  " +beverage.flavors;
+
+  const fan_favoriteP = document.createElement("p");
+  beverageDetails.append(fan_favoriteP);
+  fan_favoriteP.innerHTML ="Fan Favorite?  " + beverage.fan_favorite;
+
+  const recommendationsP = document.createElement("p");
+  beverageDetails.append(recommendationsP);
+  recommendationsP.innerHTML = "Recommendations:  "+beverage.recommendation;
+  
+ 
+    eLink.onclick = (e) => {
+    e.preventDefault();
+    document.querySelector(".dialog").classList.remove("transparent");
+    document.getElementById("modal-background").classList.remove("hidden");
+    document.getElementById("add-edit-title").innerHTML = "Edit Beverage";
+  };
+
+  dLink.onclick = (e) => {
+    e.preventDefault();
+    deleteBeverage(beverage.beverageTitle);
+  };
+
+  populateEditForm(beverage);
+};
+
+  
+  const populateEditForm = (beverage) => {
+    const form = document.getElementById("add-edit-beverage-form");
+    form.beverageId.value=beverage.beverageId,
+    form.beverageTitle.value = beverage.beverageTitle;
+    form.hot_or_iced.value = beverage.hot_or_iced;
+    form.price.value = beverage.price;
+  
+    const flavorsP = document.getElementById("flavors-boxes");
+    flavorsP.innerHTML = "";
+    console.log(beverage.flavors);
+
+    const recommendationsP = document.getElementById("recommendation-boxes");
+    recommendationsP.innerHTML = "";
+    console.log(beverage.recommendation);
+    
+    for (let i in beverage.flavors) {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = beverage.flavors[i];
+        flavorsP.append(input);
+      }
+  
+      for (let i in beverage.recommendation) {
+          const input = document.createElement("input");
+          input.type = "text";
+          input.value = beverage.recommendation[i];
+          recommendationsP.append(input);
+        }
+  };
+
+  const addEditBeverage = async (e) => {
+    e.preventDefault();
+  
+    const form = document.getElementById("add-edit-beverage-form");
+    const formData = new FormData(form);
+    formData.append("flavors", getFlavors());
+
+    console.log(...formData);
+    
+    let response;
+
+    //new beverage
+    if (form.beverageId.value==-1){
+        formData.delete("beverageId");
+        formData.delete("img");
+
+        response = await fetch("/api/beverages", {
+            method : "POST",
+            body : formData,
+        });
+    }
+    
+    if(response.status != 200){
+        console.log("Error contacting server!");
+        return;
+    }
+
+    document.querySelector(".dialog").classList.add("transparent");
+    resetForm();
     showBeverages();
 };
+
+const getFlavors = () => {
+    const inputs = document.querySelectorAll("#flavor-boxes input");
+    const flavors = [];
+  
+    inputs.forEach((input) => {
+      flavors.push(input.value);
+    });
+    console.log(flavors);
+    return flavors;
+  };
+
+
+
+  const resetForm = () => {
+    const form = document.getElementById("add-edit-beverage-form");
+    form.reset();
+    form.beverageTitleitle = "Null";
+  };
+  const showHideAdd = (e) => {
+    e.preventDefault();
+    document.querySelector(".dialog").classList.remove("transparent");
+    document.getElementById("add-edit-title").innerHTML = "Add Beverage";
+    resetForm();
+  };
+//creates input boxes
+  const addFlavors = (e) => {
+    e.preventDefault();
+    console.log("adding flavor");//test to see if works
+    const flavor_boxes = document.getElementById("flavors-boxes");
+    const input = document.createElement("input");
+    input.type = "text";
+    flavor_boxes.append(input);
+  };
+//creates input boxes
+  const addRecommendation = (e) => {
+    e.preventDefault();
+    console.log("adding recommendation");
+    const recommendation_boxes = document.getElementById("recommendation-boxes");
+    const input = document.createElement("input");
+    input.type = "text";
+    recommendation_boxes.append(input);
+  };
+
+window.onload = () => {
+    showBeverages();
+    document.getElementById("add-edit-beverage-form").onsubmit = addEditBeverage;
+    document.getElementById("add-link").onclick = showHideAdd;
+    document.getElementById("add-flavor").onclick = addFlavors;
+    document.getElementById("add-recommendation").onclick = addRecommendation;
+  
+    document.querySelector(".close").onclick = () => {
+      document.querySelector(".dialog").classList.add("transparent");
+    };
+  };
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 //Carolina Turner CSCE 242
 const express = require("express");
 const app = express();
-
-
+const Joi = require("joi");
+const multer = require("multer");
 app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+const upload = multer({dest : __dirname + "/public/images"});
 
 app.get("/" , (req,res)=>{
-    res.sendFile(__dirtitle+ "/index.html");
+    res.sendFile(__dirname+ "/index.html");
 });
 
-app.get("/api/beverages", (req, res) => {
-    const beverages = [];
-    beverages[0]={
+let beverages = [
+    {
+        beverageId : 1,
         title :"Water",
         hot_or_iced:"Iced",
         price:"$0.00",
@@ -19,55 +24,103 @@ app.get("/api/beverages", (req, res) => {
         recommendation: "N/A",
         flavors: ["N/A"],
         img: "images/water.jpg"
-    };
-    beverages[1]={
+    },
+    {   
+        beverageId : 2,
         title :"Coffee",
         hot_or_iced:"Hot",
         price:"$4.00",
         fan_favorite:"Yes",
         recommendation: "Have your coffee with cream and sugar.",
-        flavors: ["Caramel", "Vanilla", "Pumpkin Spice", "Mocha"],
+        flavors: [" Caramel", " Vanilla", " Pumpkin Spice", "Mocha"],
         img: "images/coffee.jpg"
-    };
-    beverages[2]={
+    },
+    {   
+        beverageId : 3,
         title :"Tea",
         hot_or_iced:"Hot",
         price:"$3.00",
         fan_favorite:"Yes",
         recommendation: "Try a green tea with lemon and honey.",
-        flavors: ["Black", "Green"],
+        flavors: [" Black", " Green"],
         img: "images/tea.jpg"
-    };
-    beverages[3]={
+    },
+    {
+        beverageId : 4,
         title :"Milkshake",
         hot_or_iced:"Iced",
         price:"$5.00",
         fan_favorite:"Yes",
         recommendation: "Add our hot fudge topping!",
-        flavors: ["Reese's Peanut Butter", "Oreo", "Classic Vanilla", "Classic Chocolate", "Peach"],
+        flavors: [" Reese's Peanut Butter", " Oreo", " Classic Vanilla", " Classic Chocolate", "Peach"],
         img: "images/milkshake.jpg"
-    };
-    beverages[4]={
+    },
+    {
+        beverageId : 5,
         title :"Soda",
         hot_or_iced:"Iced",
         price:"2 for $3.00",
         fan_favorite:"No",
         recommendation: "N/A",
-        flavors: ["Coca-Cola","Mr.Pibb", "Sprite", "Dr.Pepper"],
+        flavors: [" Coca-Cola"," Mr.Pibb", " Sprite", " Dr.Pepper"],
         img: "images/soda.jpg"
-    };
-    beverages[5]={
+    },
+    {
+        beverageId : 6,
         title :"Celsius",
         hot_or_iced:"Iced",
         price:"$3.00",
         fan_favorite:"Yes",
         recommendation: "Raspberry Acai Green Tea Flavor",
-        flavors: ["Raspberry Acai Green Tea","Kiwi Guava", "Berry", "Orange"],
+        flavors: [" Raspberry Acai Green Tea"," Kiwi Guava", " Berry", " Orange"],
         img: "images/celsius.jpg"
-    };
-      
-    res.json(beverages);
+    }
+];
+
+
+app.get("/api/beverages", (req, res) =>{  
+    res.send(beverages);
   });
+
+
+app.post("/api/beverages", upload.single("img"), (req,res)=>{
+    const result = validateBeverage(req.body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const beverage = {
+        beverageId :beverages.length+1,
+        title : req.body.title,
+        hot_or_iced : req.body.hot_or_iced,
+        price : req.body.price,
+        fan_favorite : req.body.fan_favorite,
+        recommendation : req.body.recommendation,
+        flavors : req.body.flavors.split(","),
+    };
+
+    beverages.push(beverage);
+    res.send(beverage);
+});
+
+const validateBeverage = (beverage)=> {
+    const schema = Joi.object({ //Joi Validation
+        beverageId : Joi.allow(""),
+        title : Joi.string().min(3).required(), //joi validating it must be string of length three and is required, "tea" is minimum length
+        hot_or_iced : Joi.string().min(3), //not required if can be either or
+        price : Joi.allow().required(),
+        fan_favorite: Joi.allow(),
+        recommendation : Joi.allow().required(),
+        flavors :Joi.allow(),
+    });
+    return schema.validate(beverage);
+  }
+
+  
+  
+
 
 app.listen(3000, ()=>{
     console.log("working!");
